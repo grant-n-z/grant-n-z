6036aee627c03882c1afac3a07c9bc00c086d14e
tomo0111 <tohito0430@gmail.com>
2018-03-24 19:00:16 +0900

    add import

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,13 +1,13 @@
 package controllers
 
-import (
-	"github.com/revel/revel"
-)
+import "github.com/revel/revel"
+import "revel-api/app/domain/model"
 
 type App struct {
 	*revel.Controller
 }
 
 func (c App) Index() revel.Result {
-	return c.Render()
+	return c.RenderJSON(model.UserModel{})
 }
+
