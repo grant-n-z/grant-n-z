a32bfa445db1ba3d7f4f329039cbb56e4065127a
tomo0111 <tohito0430@gmail.com>
2018-04-09 00:06:50 +0900

    add gorm

--- app/gorm.go
+++ app/gorm.go
@@ -1,1 +1,47 @@
 package app
+
+import (
+	"github.com/jinzhu/gorm"
+	r "github.com/revel/revel"
+	"database/sql"
+)
+
+type GormController struct {
+	*r.Controller
+	Txn *gorm.DB
+}
+
+var Gdb *gorm.DB
+
+func (c *GormController) Begin() r.Result {
+	txn := Gdb.Begin()
+	if txn.Error != nil {
+		panic(txn.Error)
+	}
+	c.Txn = txn
+	return nil
+}
+
+func (c *GormController) Commit() r.Result {
+	if c.Txn == nil {
+		return nil
+	}
+	c.Txn.Commit()
+	if err := c.Txn.Error; err != nil && err != sql.ErrTxDone {
+		panic(err)
+	}
+	c.Txn = nil
+	return nil
+}
+
+func (c *GormController) Rollback() r.Result {
+	if c.Txn == nil {
+		return nil
+	}
+	c.Txn.Rollback()
+	if err := c.Txn.Error; err != nil && err != sql.ErrTxDone {
+		panic(err)
+	}
+	c.Txn = nil
+	return nil
+}
