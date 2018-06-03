447007e66594723201575075c6d406b8852d6af5
tomo0111 <tohito0430@gmail.com>
2018-06-03 18:13:08 +0900

    reafactor

--- tests/apptest.go
+++ tests/apptest.go
@@ -8,6 +8,12 @@ type AppTest struct {
 	testing.TestSuite
 }
 
+// Run this before a request
 func (t *AppTest) Before() {
 	println("Set up")
+}
+
+// Run this after request
+func (t *AppTest) After() {
+	println("Tear down")
 }
