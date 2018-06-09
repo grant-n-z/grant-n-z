0ea92cc268f907e84fbcbcfe036a9ea9e2f7abe5
tomo0111 <tohito0430@gmail.com>
2018-06-10 01:00:48 +0900

    add validator

--- app/controllers/v1/user_controller.go
+++ app/controllers/v1/user_controller.go
@@ -3,30 +3,35 @@ package v1
 import (
 	"authentication-server/app/domains/entity"
 	"authentication-server/app/controllers"
-	"authentication-server/app/domains/service"
 	"github.com/satori/go.uuid"
 	"github.com/revel/revel"
+	"gopkg.in/go-playground/validator.v9"
+	"authentication-server/app/domains/service"
 )
 
 type UserController struct {
 	controllers.BaseApiController
+	validate *validator.Validate
 }
 
-func (c UserController) PostUser() revel.Result {
+var validate = validator.New()
 
-	users := entity.Users {
-		Uuid: uuid.Must(uuid.NewV4()).String(),
-	}
+func (c UserController) PostUser(users entity.Users) revel.Result {
 
-	if err := c.Params.BindJSON(&users); err != nil {
+	users.Uuid = uuid.Must(uuid.NewV4()).String()
+
+	err := validate.Struct(users)
+	if err != nil {
 		return c.BadRequest("001")
 	}
 
-	// TODO: ユーザ存在確認
-
 	var useService service.UserService
+	if useService.GetUserByEmail(users.Email).Response == nil {
+		return c.NotFound("002")
+	}
+	
 	if useService.InsertUser(users) == false {
-		return c.InternalServer("002")
+		return c.InternalServer("003")
 	}
 
 	return c.RenderJSON(users)
