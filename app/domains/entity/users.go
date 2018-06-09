0341eac456e60f75c97d925ea103a5a1978230df
tomo0111 <tohito0430@gmail.com>
2018-06-10 01:00:58 +0900

    add validator

--- app/domains/entity/users.go
+++ app/domains/entity/users.go
@@ -3,11 +3,11 @@ package entity
 import "time"
 
 type Users struct {
-	Id int `json:"id"`
-	Uuid string `json:"uuid"`
-	Username string `json:"username"`
-	Email string `json:"email"`
-	Password string `json:"password"`
+	Id        int       `json:"id"`
+	Uuid      string    `json:"uuid"`
+	Username  string    `validate:"required"json:"username"`
+	Email     string    `validate:"required,email"json:"email"`
+	Password  string    `validate:"required"json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 }
 
