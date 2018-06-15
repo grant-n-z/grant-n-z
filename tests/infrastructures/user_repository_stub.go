dda3a8ce51d837b8c1a9e9f6f92d8bb40c0ad307
tomo0111 <tohito0430@gmail.com>
2018-06-15 23:02:27 +0900

    add repository stub

---
+++ tests/infrastructures/user_repository_stub.go
@@ -0,0 +1,33 @@
+package infrastructures
+
+import (
+	"authentication-server/app/infrastructures"
+	"authentication-server/app/domains/entity"
+	"github.com/satori/go.uuid"
+)
+
+type UserRepositoryStub struct {}
+
+func (r UserRepositoryStub) NewUserRepository() infrastructures.UserRepository {
+	return &UserRepositoryStub{}
+}
+
+func (r UserRepositoryStub) FindByEmail(email string) *entity.Users {
+	users := entity.Users{
+		Id: 1,
+		Uuid: uuid.Must(uuid.NewV4()).String(),
+		Username: "test",
+		Email: email,
+		Password: "testtest",
+	}
+
+	return &users
+}
+
+func (r UserRepositoryStub) Save(users entity.Users) *entity.Users {
+	return &users
+}
+
+func (r UserRepositoryStub) Update(users entity.Users) *entity.Users {
+	return &users
+}
