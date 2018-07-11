9ebbf075206ed7dbf4afc7af4ddd3e8d621ab4e0
tomo0111 <tohito0430@gmail.com>
2018-07-11 23:29:20 +0900

    refactor import

--- app/infrastructures/user_repository.go
+++ app/infrastructures/user_repository.go
@@ -1,6 +1,6 @@
 package infrastructures
 
-import "grant-n-z/app/domains/entity"
+import "github.com/tomo0111/grant-n-z/app/domains/entity"
 
 type UserRepository interface {
 	FindByEmail(email string) *entity.Users
