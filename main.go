f033db2ff476cf17e2fb76c3a6dc650b1011438d
tomoyane <tohito0430@gmail.com>
2018-08-11 13:01:03 +0900

    add validator method

--- main.go
+++ main.go
@@ -4,13 +4,15 @@ import (
 	"github.com/labstack/echo"
 	"github.com/tomoyane/grant-n-z/common"
 	"github.com/tomoyane/grant-n-z/controller"
+	"gopkg.in/go-playground/validator.v9"
+	"github.com/tomoyane/grant-n-z/domain"
 )
 
 func main() {
 	common.InitDB()
+
 	e := echo.New()
-
+	e.Validator = &domain.GrantValidator{Validator: validator.New()}
 	e.POST("/v1/users", controller.UserController)
-
 	e.Logger.Fatal(e.Start(":8080"))
 }
