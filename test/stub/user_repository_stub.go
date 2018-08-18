a2c0469eb58fb0de0ba33a712174069ebcc1eb74
tomoyane <tohito0430@gmail.com>
2018-08-19 01:32:33 +0900

    impl stub repository

---
+++ test/stub/user_repository_stub.go
@@ -0,0 +1,28 @@
+package stub
+
+import (
+	"github.com/tomoyane/grant-n-z/domain/entity"
+	"github.com/satori/go.uuid"
+)
+
+type UserRepositoryStub struct {
+}
+
+func (r UserRepositoryStub) FindByEmail(email string) *entity.User {
+	users := entity.User{
+		Username: "test",
+		Email: email,
+		Uuid: uuid.NewV4(),
+		Password: "test12345",
+	}
+	return &users
+}
+
+func (r UserRepositoryStub) Save(user entity.User) *entity.User {
+	return &user
+}
+
+func (r UserRepositoryStub) Update(user entity.User) *entity.User {
+	return &user
+}
+
