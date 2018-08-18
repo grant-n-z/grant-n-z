0037d177f516a5d08f841c62a5451fbb2328eaf4
tomoyane <tohito0430@gmail.com>
2018-08-19 01:30:27 +0900

    impl user di configuration

--- common/config.go
+++ common/config.go
@@ -1,50 +1,17 @@
 package common
 
 import (
-	"github.com/jinzhu/gorm"
 	_ "github.com/go-sql-driver/mysql"
-	"os"
-	"io/ioutil"
-	"gopkg.in/yaml.v2"
-	"github.com/tomoyane/grant-n-z/domain"
-	"fmt"
+	"github.com/tomoyane/grant-n-z/domain/service"
+	"github.com/tomoyane/grant-n-z/domain/repository"
 )
 
 var (
-	Db *gorm.DB
-	dbSource string
-	yml domain.YmlModel
+	ProvideUserService service.UserService
 )
 
-func InitDB() {
-	switch os.Getenv("ENV") {
-	case "test":
-		yml = readYml("../app-test.yaml")
-		dbSource = yml.GetDataSourceUrl()
-	default:
-		yml = readYml("app.yaml")
-		dbSource = yml.GetDataSourceUrl()
+func InitUserService(repo repository.UserRepository) {
+	ProvideUserService = service.UserService{
+		UserRepository: repo,
 	}
-
-	fmt.Print(dbSource)
-
-	db, err := gorm.Open("mysql", dbSource)
-	if err != nil {
-		panic(err)
-	}
-
-	db.DB()
-	Db = db
-}
-
-func readYml(ymlName string) domain.YmlModel {
-	yml, err := ioutil.ReadFile(ymlName)
-	if err != nil {
-		panic(err)
-	}
-
-	var db domain.YmlModel
-	err = yaml.Unmarshal(yml, &db)
-
-	return db
 }
