c8ff9652f6c1698437ef207cfea09f87830d07f4
tomoyane <tohito0430@gmail.com>
2018-08-19 01:32:08 +0900

    impl main test

--- test/main_test.go
+++ test/main_test.go
@@ -2,41 +2,13 @@ package test
 
 import (
 	"testing"
-	"github.com/tomoyane/embedded-mysql-container/container"
 	"os"
 	"github.com/tomoyane/grant-n-z/common"
-	"time"
+	"github.com/tomoyane/grant-n-z/test/stub"
 )
 
 func TestMain(m *testing.M) {
-	os.Setenv("ENV", "test")
-	containerDaemon := container.ContainerDaemonImpl{}.New()
-	embeddedMysql := container.MysqlConfigImpl{}.New()
-
-	containerId := containerDaemon.StartEmbeddedMysql()
-
-	time.Sleep(10 * time.Second)
-
-	embeddedMysql.AddSchema("auth_server")
-	embeddedMysql.CreateTable("CREATE TABLE auth_server.users (" +
-		"id int(11) NOT NULL AUTO_INCREMENT," +
-		"uuid varchar(128) NOT NULL," +
-		"username varchar(128) NOT NULL," +
-		"email varchar(128) NOT NULL," +
-		"password varchar(128) NOT NULL," +
-		"created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP," +
-		"updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP," +
-		"PRIMARY KEY (id)," +
-		"UNIQUE (email))" +
-		" ENGINE=InnoDB DEFAULT CHARSET=utf8")
-
-	// Init database
-	common.InitDB()
-
+	common.InitUserService(stub.UserRepositoryStub{})
 	code := m.Run()
-
-	// After
-	containerDaemon.FinishEmbeddedMysql(containerId)
-
 	os.Exit(code)
 }
