51e39736b93ff7970cb6b038f28dbb850f04a9d4
tomoyane <tohito0430@gmail.com>
2018-08-23 22:38:14 +0900

    impl token service

---
+++ domain/service/token_service.go
@@ -0,0 +1,36 @@
+package service
+
+import (
+	"golang.org/x/crypto/bcrypt"
+	"github.com/tomoyane/grant-n-z/domain/entity"
+	"github.com/tomoyane/grant-n-z/domain/repository"
+)
+
+type TokenService struct {
+	UserRepository repository.UserRepository
+}
+
+func (u UserService) EncryptPw(password string) string {
+	hash, _ := bcrypt.GenerateFromPassword(
+		[] byte(password),
+		bcrypt.DefaultCost,
+	)
+	return string(hash)
+}
+
+func (u UserService) ComparePw(passwordHash string, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(password))
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (u UserService) GetUserByEmail(email string) *entity.User {
+	return u.UserRepository.FindByEmail(email)
+}
+
+func (u UserService) InsertUser(user entity.User) *entity.User {
+	return u.UserRepository.Save(user)
+}
