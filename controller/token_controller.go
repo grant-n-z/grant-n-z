635759cebec3446c6ad797bb4263fdadd00b4628
tomoyane <tohito0430@gmail.com>
2018-08-27 00:14:21 +0900

    impl token controller

--- controller/token_controller.go
+++ controller/token_controller.go
@@ -2,51 +2,13 @@ package controller
 
 import (
 	"github.com/labstack/echo"
-	"github.com/tomoyane/grant-n-z/domain/entity"
 	"net/http"
-	"github.com/tomoyane/grant-n-z/domain"
-	"github.com/satori/go.uuid"
-	"strings"
-	"github.com/tomoyane/grant-n-z/common"
-	"github.com/tomoyane/grant-n-z/infra"
 )
 
-func GetToken(c echo.Context) (err error) {
-	user := new(entity.User)
-
-	if err = c.Bind(user); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest,
-			domain.ErrorResponse{}.Error(http.StatusBadRequest, "001"))
-	}
-
-	user.Uuid = uuid.NewV4()
-	user.Password = common.ProvideUserService.EncryptPw(user.Password)
-
-	if err = c.Validate(user); err != nil {
-		return echo.NewHTTPError(http.StatusBadRequest,
-			domain.ErrorResponse{}.Error(http.StatusBadRequest, "002"))
-	}
-
-	userData := common.ProvideUserService.GetUserByEmail(user.Email)
-	if userData == nil {
-		return echo.NewHTTPError(http.StatusInternalServerError,
-			domain.ErrorResponse{}.Error(http.StatusInternalServerError, "003"))
-	}
-
-	if !strings.EqualFold(userData.Email, "") {
-		return echo.NewHTTPError(http.StatusUnprocessableEntity,
-			domain.ErrorResponse{}.Error(http.StatusUnprocessableEntity, "004"))
-	}
-
-	if common.ProvideUserService.InsertUser(*user) == nil {
-		return echo.NewHTTPError(http.StatusInternalServerError,
-			domain.ErrorResponse{}.Error(http.StatusInternalServerError, "005"))
-	}
-
+func PostToken(c echo.Context) (err error) {
 	success := map[string]string {
 		"message": "user creation succeeded.",
 	}
 
-	c.Response().Header().Add("Location", infra.GetHostName() + "/v1/users/" + user.Uuid.String())
 	return c.JSON(http.StatusCreated, success)
 }
