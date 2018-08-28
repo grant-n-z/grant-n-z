dbd2602a506704fd69be1b78b02499cd304a220f
tomoyane <tohito0430@gmail.com>
2018-08-28 19:28:00 +0900

    impl controller main test

---
+++ test/controller/main_test.go
@@ -0,0 +1,30 @@
+package controller
+
+import (
+	"github.com/labstack/echo"
+	"github.com/tomoyane/grant-n-z/di"
+	"github.com/tomoyane/grant-n-z/test/stub"
+	"github.com/tomoyane/grant-n-z/domain"
+	"gopkg.in/go-playground/validator.v9"
+	"fmt"
+	"os"
+	"testing"
+)
+
+var(
+	e = echo.New()
+)
+
+func TestMain(m *testing.M) {
+	di.InitUserService(stub.UserRepositoryStub{})
+	e.Validator = &domain.GrantValidator{Validator: validator.New()}
+
+	code := m.Run()
+
+	fmt.Println("Done controller packge")
+
+	os.Exit(code)
+}
+
+
+
