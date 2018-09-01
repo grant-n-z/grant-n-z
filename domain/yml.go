13adb9f4e163f4eb0b651f5350a760b8b48ba2e8
tomoyane <tohito0430@gmail.com>
2018-09-02 00:53:48 +0900

    impl env variable

--- domain/yml.go
+++ domain/yml.go
@@ -1,6 +1,10 @@
 package domain
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 type Yml struct {
 	DbModel DbModel `yaml:"db"`
@@ -15,10 +19,31 @@ type DbModel struct {
 }
 
 func (yml Yml) GetDataSourceUrl() string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True",
-		yml.DbModel.User,
-		yml.DbModel.Password,
-		yml.DbModel.Host,
-		yml.DbModel.Port,
-		yml.DbModel.Db)
+	user := yml.DbModel.User
+	pass := yml.DbModel.Password
+	host := yml.DbModel.Host
+	port := yml.DbModel.Port
+	db := yml.DbModel.Db
+
+	if strings.Contains(user, "$") {
+		user = os.Getenv(yml.DbModel.User[1:])
+	}
+
+	if strings.Contains(pass, "$") {
+		pass = os.Getenv(yml.DbModel.Password[1:])
+	}
+
+	if strings.Contains(host, "$") {
+		host = os.Getenv(yml.DbModel.Host[1:])
+	}
+
+	if strings.Contains(port, "$") {
+		port = os.Getenv(yml.DbModel.Port[1:])
+	}
+
+	if strings.Contains(db, "$") {
+		db = os.Getenv(yml.DbModel.Db[1:])
+	}
+
+	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True", user, pass, host, port, db)
 }
