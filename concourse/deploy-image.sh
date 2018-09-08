766e4486fa27f4c047091a1e1bcddbfe46229343
tomoyane <tohito0430@gmail.com>
2018-09-08 16:14:09 +0900

    fix touch command

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -2,13 +2,13 @@
 
 set -e -u -x
 
-touch ../out/version.txt
-
 go get github.com/tomoyane/grant-n-z
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/stretchr/testify/assert
 
 cd /go/src/github.com/tomoyane/grant-n-z
+
+touch ../out/version.txt
 
 cat app.yaml | grep version |sed -e 's/[^0-9.]//g' > ../out/version.txt
 
