464446d00c63534ebbec383a13ee7353fd82d31d
tomoyane <tohito0430@gmail.com>
2018-09-08 14:59:29 +0900

    fix test shell

--- concourse/test.sh
+++ concourse/test.sh
@@ -6,10 +6,11 @@ go get -u github.com/golang/dep/cmd/dep
 
 mkdir /go/src/github.com/tomoyane/
 
-cp -r repository /go/src/github.com/tomoyane/
+mv repository grant-n-z
+cp -r grant-n-z /go/src/github.com/tomoyane/
 
-cd /go/src/github.com/tomoyane/repository
+cd /go/src/github.com/tomoyane/grant-n-z
 
 dep ensure
 
-go test -v github.com/tomoyane/repository/test/...
+go test -v github.com/tomoyane/grant-n-z/test/...
