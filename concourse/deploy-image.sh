79b9c012f16aeca153a6a3fe779bad1d61d54702
tomoyane <tohito0430@gmail.com>
2018-09-08 16:37:57 +0900

    fix pipeline

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -8,10 +8,7 @@ go get github.com/stretchr/testify/assert
 
 cd /go/src/github.com/tomoyane/grant-n-z
 
-mkdir /repository/out
-touch /repository/out/version.txt
-
-cat app.yaml | grep version |sed -e 's/[^0-9.]//g' > /repository/out/version.txt
+cat app.yaml | grep version |sed -e 's/[^0-9.]//g'
 
 dep ensure
 
