753594308de2709cfffe596992451c16c5df872a
tomoyane <tohito0430@gmail.com>
2018-09-08 15:26:04 +0900

    use go get

--- concourse/test.sh
+++ concourse/test.sh
@@ -2,14 +2,9 @@
 
 set -e -u -x
 
-go get github.com/package/path
+go get github.com/tomoyane/grant-n-z
 go get -u github.com/golang/dep/cmd/dep
 
-mkdir /go/src/github.com/tomoyane/
-
-cp -r repository /go/src/github.com/tomoyane/
-
-mv /go/src/github.com/tomoyane/repository /go/src/github.com/tomoyane/grant-n-z
 cd /go/src/github.com/tomoyane/grant-n-z
 
 dep ensure
