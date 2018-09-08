df7bb84e7fc923d96ff43648146832baef80b5dc
tomoyane <tohito0430@gmail.com>
2018-09-08 14:53:33 +0900

    fix test sh

--- concourse/test.sh
+++ concourse/test.sh
@@ -4,6 +4,8 @@ set -e -u -x
 
 go get -u github.com/golang/dep/cmd/dep
 
+mkdir /go/src/github.com/tomoyane/
+
 cp -r repository /go/src/github.com/tomoyane/
 
 cd /go/src/github.com/tomoyane/repository
