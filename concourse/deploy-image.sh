ea9393238b064285e69f4de0fbad6b8baf516703
tomoyane <tohito0430@gmail.com>
2018-09-08 16:07:17 +0900

    fix deploy

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -4,11 +4,14 @@ set -e -u -x
 
 touch ../out/version.txt
 
-ls -la
+go get github.com/tomoyane/grant-n-z
+go get -u github.com/golang/dep/cmd/dep
+go get github.com/stretchr/testify/assert
+
+cd /go/src/github.com/tomoyane/grant-n-z
 
 cat app.yaml | grep version |sed -e 's/[^0-9.]//g' > ../out/version.txt
 
-go get -u github.com/golang/dep/cmd/dep
-
 dep ensure
+
 go build
