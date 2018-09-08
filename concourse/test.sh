ba95f2eb1ff45694466287fb1ef9777b80ee11d3
tomoyane <tohito0430@gmail.com>
2018-09-08 14:47:03 +0900

    test pipeline

--- concourse/test.sh
+++ concourse/test.sh
@@ -4,12 +4,13 @@ set -e -u -x
 
 go get -u github.com/golang/dep/cmd/dep
 
-cp -r repository /go/src/
+cp -r repository /go/src/github.com/tomoyane/
 
-cd /go/src/repository
+cd /go/src/github.com/tomoyane/
 
 dep ensure
 
 pwd
 ls -la
-go test -v /go/src/repository/test/...
+
+go test -v go test -v github.com/tomoyane/repository/test/...
