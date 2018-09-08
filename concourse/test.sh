9c32e22036ec887ac073e818bfa49fd3c0605fab
tomoyane <tohito0430@gmail.com>
2018-09-08 15:22:41 +0900

    concourse test

--- concourse/test.sh
+++ concourse/test.sh
@@ -2,6 +2,7 @@
 
 set -e -u -x
 
+go get github.com/package/path
 go get -u github.com/golang/dep/cmd/dep
 
 mkdir /go/src/github.com/tomoyane/
@@ -10,10 +11,6 @@ cp -r repository /go/src/github.com/tomoyane/
 
 mv /go/src/github.com/tomoyane/repository /go/src/github.com/tomoyane/grant-n-z
 cd /go/src/github.com/tomoyane/grant-n-z
-
-export GOPATH=/go/src
-
-echo $GOPATH
 
 dep ensure
 
