d4dd0469321a6bb76aaa97e086cf866fce05ae19
tomoyane <tohito0430@gmail.com>
2018-09-08 15:57:50 +0900

    ifix deploy yml

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -3,6 +3,9 @@
 set -e -u -x
 
 touch ../out/version.txt
-cat cat app.yaml | grep version |sed -e 's/[^0-9.]//g' > ../out/version.txt
+
+ls -la
+
+cat app.yaml | grep version |sed -e 's/[^0-9.]//g' > ../out/version.txt
 
 go build
