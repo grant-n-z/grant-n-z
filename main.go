07c7532fba8bc67d3affa62c44b4ab75b142a72a
tomoyane <tohito0430@gmail.com>
2018-09-09 14:19:36 +0900

    impl main

--- main.go
+++ main.go
@@ -11,15 +11,18 @@ import (
 )
 
 func main() {
+	infra.InitYaml()
 	infra.InitDB()
 	infra.MigrateDB()
 
 	di.InitUserService(repository.UserRepositoryImpl{})
 	di.InitTokenService(repository.TokenRepositoryImpl{})
+	di.InitRoleService(repository.RoleRepositoryImpl{})
 
 	e := echo.New()
 	e.Validator = &domain.GrantValidator{Validator: validator.New()}
 	e.POST("/v1/users", controller.PostUser)
 	e.POST("/v1/tokens", controller.PostToken)
+	e.POST("/v1/grants", controller.GrantToken)
 	e.Logger.Fatal(e.Start(":8080"))
 }
