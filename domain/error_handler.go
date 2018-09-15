3caa705704c20d4dacdfa6955be947915670468d
tomoyane <tohito0430@gmail.com>
2018-09-15 14:15:18 +0900

    refactor error struct

---
+++ domain/error_handler.go
@@ -0,0 +1,66 @@
+package domain
+
+import (
+	"net/http"
+	"fmt"
+)
+
+type ErrorResponse struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+	Detail  string `json:"detail"`
+}
+
+func BadRequest(internalCode string) ErrorResponse {
+	return ErrorResponse{
+		Code: http.StatusBadRequest,
+		Detail: internalCode,
+		Message: "Bad request.",
+	}
+}
+
+func Unauthorized(internalCode string) ErrorResponse {
+	return ErrorResponse{
+		Code: http.StatusUnauthorized,
+		Detail: internalCode,
+		Message: "Unauthorized.",
+	}
+}
+
+func Forbidden(internalCode string) ErrorResponse {
+	return ErrorResponse{
+		Code: http.StatusForbidden,
+		Detail: internalCode,
+		Message: "Forbidden.",
+	}
+}
+
+func NotFound(internalCode string) ErrorResponse {
+	return ErrorResponse{
+		Code: http.StatusNotFound,
+		Detail: internalCode,
+		Message: "Not found.",
+	}
+}
+
+func UnProcessableEntity(internalCode string) ErrorResponse {
+	return ErrorResponse{
+		Code: http.StatusUnprocessableEntity,
+		Detail: internalCode,
+		Message: "UnProcessable Entity.",
+	}
+}
+
+func InternalServerError(internalCode string) ErrorResponse {
+	return ErrorResponse{
+		Code: http.StatusInternalServerError,
+		Detail: internalCode,
+		Message: "Internal server error.",
+	}
+}
+
+func (e ErrorResponse) Print(code int, msg string, detail string) {
+	fmt.Print(code)
+	fmt.Print(msg)
+	fmt.Print(detail)
+}
