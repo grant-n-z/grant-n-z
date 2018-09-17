5ada3af49c3b092e19282a11e1fa7f108c221a30
tomoyane <tohito0430@gmail.com>
2018-09-17 19:42:47 +0900

    impl token repository stub

---
+++ test/stub/token_repository_stub.go
@@ -0,0 +1,25 @@
+package stub
+
+import (
+	"github.com/tomoyane/grant-n-z/domain/entity"
+	"github.com/satori/go.uuid"
+)
+
+type TokenRepositoryStub struct {
+}
+
+func (r TokenRepositoryStub) FindByUserUuid(userUuidStr string) *entity.Token {
+	userUuid, _ := uuid.FromString(userUuidStr)
+	token := entity.Token{
+		Id: 1,
+		TokenType: "Bearer",
+		Token: "testToken",
+		RefreshToken: "testRefreshToken",
+		UserUuid: userUuid,
+	}
+	return &token
+}
+
+func (r TokenRepositoryStub) Save(token entity.Token) *entity.Token {
+	return &token
+}
