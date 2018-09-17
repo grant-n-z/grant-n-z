14fa73d334c7ae7bfc8fd2d9c9c921c0162b6094
tomoyane <tohito0430@gmail.com>
2018-09-17 19:42:40 +0900

    impl token service test

---
+++ test/domain/token_service_test.go
@@ -0,0 +1,56 @@
+package domain
+
+import (
+	"testing"
+	"github.com/tomoyane/grant-n-z/di"
+	"github.com/stretchr/testify/assert"
+	"github.com/tomoyane/grant-n-z/domain/entity"
+)
+
+func TestGenerateJwt(t *testing.T) {
+	assert.NotEmpty(t, di.ProviderTokenService.GenerateJwt(username, userUuid, false))
+}
+
+func TestParseJwtOk(t *testing.T) {
+	token := di.ProviderTokenService.GenerateJwt(username, userUuid, false)
+	_, result := di.ProviderTokenService.ParseJwt(token)
+
+	assert.Equal(t, result, true)
+}
+
+func TestParseJwtInValid01(t *testing.T) {
+	_, result := di.ProviderTokenService.ParseJwt("test")
+
+	assert.Equal(t, result, false)
+}
+
+func TestParseJwtInValid02(t *testing.T) {
+	_, result := di.ProviderTokenService.ParseJwt(token)
+
+	assert.Equal(t, result, false)
+}
+
+func TestGetTokenByUserUuid(t *testing.T) {
+	userUuidStr := "52F6228E-9169-4563-ADE2-07ED697B67BA"
+	token := di.ProviderTokenService.GetTokenByUserUuid(userUuidStr)
+
+	assert.Equal(t, token.TokenType, "Bearer")
+	assert.Equal(t, token.Token, "testToken")
+	assert.Equal(t, token.RefreshToken, "testRefreshToken")
+	assert.Equal(t, token.UserUuid, userUuid)
+}
+
+func TestInsertToken(t *testing.T) {
+	token := entity.Token {
+		Id: 1,
+		TokenType: "Bearer",
+		Token: "testToken",
+		RefreshToken: "testRefreshToken",
+		UserUuid: userUuid,
+	}
+
+	assert.Equal(t, token.TokenType, "Bearer")
+	assert.Equal(t, token.Token, "testToken")
+	assert.Equal(t, token.RefreshToken, "testRefreshToken")
+	assert.Equal(t, token.UserUuid, userUuid)
+}
