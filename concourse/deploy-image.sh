ad5d3a7511b8aa29d758458ccf8e62f0e8d6aa52
tomoyane <tohito0430@gmail.com>
2018-09-22 13:24:01 +0900

    test pipeline

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -2,6 +2,8 @@
 
 set -e -u -x
 
+ls -la 
+
 go get github.com/tomoyane/grant-n-z
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/stretchr/testify/assert
@@ -9,6 +11,7 @@ go get github.com/stretchr/testify/assert
 cd /go/src/github.com/tomoyane/grant-n-z
 
 ls -la /
+
 touch /version/version.txt
 cat app.yaml | grep version |sed -e 's/[^0-9.]//g' >> /version/version.txt
 
