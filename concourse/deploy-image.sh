a64d486c4f7c84184d41544bd208c924fcbd09fa
tomoyane <tohito0430@gmail.com>
2018-09-22 13:43:25 +0900

    add version

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -2,16 +2,14 @@
 
 set -e -u -x
 
-ls -la ../
-
 go get github.com/tomoyane/grant-n-z
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/stretchr/testify/assert
 
 cd /go/src/github.com/tomoyane/grant-n-z
 
-touch /version/version.txt
-cat app.yaml | grep version |sed -e 's/[^0-9.]//g' >> /version/version.txt
+touch ../version/version.txt
+cat app.yaml | grep version |sed -e 's/[^0-9.]//g' >> ../version/version.txt
 
 dep ensure
 
