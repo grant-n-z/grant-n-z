41349c2b615c659a662daa6913bb59b6343e5f30
tomoyane <tohito0430@gmail.com>
2018-09-22 13:47:29 +0900

    fix pipeline docker image tag version

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -2,14 +2,14 @@
 
 set -e -u -x
 
+touch ../version/version.txt
+cat app.yaml | grep version |sed -e 's/[^0-9.]//g' >> ../version/version.txt
+
 go get github.com/tomoyane/grant-n-z
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/stretchr/testify/assert
 
 cd /go/src/github.com/tomoyane/grant-n-z
-
-touch ../version/version.txt
-cat app.yaml | grep version |sed -e 's/[^0-9.]//g' >> ../version/version.txt
 
 dep ensure
 
