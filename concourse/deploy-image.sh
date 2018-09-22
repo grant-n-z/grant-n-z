e3909e61bb9d33c2b71380501856f57bcd8fbc95
tomoyane <tohito0430@gmail.com>
2018-09-22 13:32:50 +0900

    test outputs

--- concourse/deploy-image.sh
+++ concourse/deploy-image.sh
@@ -2,15 +2,13 @@
 
 set -e -u -x
 
-ls -la 
+ls -la ../
 
 go get github.com/tomoyane/grant-n-z
 go get -u github.com/golang/dep/cmd/dep
 go get github.com/stretchr/testify/assert
 
 cd /go/src/github.com/tomoyane/grant-n-z
-
-ls -la /
 
 touch /version/version.txt
 cat app.yaml | grep version |sed -e 's/[^0-9.]//g' >> /version/version.txt
