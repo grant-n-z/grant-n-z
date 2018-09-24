12f0bbdbe43c98bc4178e1c57b81d2114c5e0b37
tomoyane <tohito0430@gmail.com>
2018-09-24 16:29:19 +0900

    add put

--- main.go
+++ main.go
@@ -1,30 +1,34 @@
 package main
 
 import (
-	"github.com/tomoyane/grant-n-z/di"
-	"github.com/tomoyane/grant-n-z/domain/repository"
 	"github.com/labstack/echo"
 	"github.com/tomoyane/grant-n-z/domain"
 	"gopkg.in/go-playground/validator.v9"
 	"github.com/tomoyane/grant-n-z/controller"
 	"github.com/tomoyane/grant-n-z/infra"
+	"github.com/tomoyane/grant-n-z/di"
+	"github.com/tomoyane/grant-n-z/domain/repository"
 )
 
 func main() {
-	infra.InitYaml()
-	infra.InitDB()
-	infra.MigrateDB()
-
 	di.InitUserService(repository.UserRepositoryImpl{})
 	di.InitTokenService(repository.TokenRepositoryImpl{})
 	di.InitRoleService(repository.RoleRepositoryImpl{})
 	di.InitPrincipalService(repository.PrincipalRepositoryImpl{})
 
+	infra.InitYaml()
+	infra.InitDB()
+	infra.MigrateDB()
+
 	e := echo.New()
 	e.Validator = &domain.GrantValidator{Validator: validator.New()}
+
 	e.POST("/v1/users", controller.PostUser)
+	e.PUT("/v1/users/:column", controller.PutUser)
+
 	e.POST("/v1/roles", controller.PostRole)
 	e.POST("/v1/principals", controller.PostPrincipal)
+
 	e.POST("/v1/tokens", controller.PostToken)
 	e.POST("/v1/grants", controller.GrantToken)
 	e.Logger.Fatal(e.Start(":8080"))
