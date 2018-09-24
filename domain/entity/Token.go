e704f73979c010fee9d5be3e1999edb30982da48
tomoyane <tohito0430@gmail.com>
2018-09-24 16:28:32 +0900

    refactor token entity

--- domain/entity/Token.go
+++ domain/entity/Token.go
@@ -10,7 +10,7 @@ type Token struct {
 	TokenType    string    `gorm:"type:varchar(128);not null"json:"token_type"`
 	Token        string    `gorm:"type:varchar(512);not null"json:"token"`
 	RefreshToken string    `gorm:"type:varchar(512);not null"json:"refresh_token"`
-	UserUuid     uuid.UUID `gorm:"type:varchar(128);index:user_uuid"json:"user_uuid"`
+	UserUuid     uuid.UUID `gorm:"type:varchar(128);not null;index:user_uuid"json:"user_uuid"`
 	CreatedAt    time.Time `json:"created_at"`
 	UpdatedAt    time.Time `json:"updated_at"`
 }
