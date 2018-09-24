5adc462e2a12fa69b8beea439621fb87f404d25d
tomoyane <tohito0430@gmail.com>
2018-09-24 16:28:46 +0900

    refactor member entity

--- domain/entity/member.go
+++ domain/entity/member.go
@@ -8,7 +8,6 @@ import (
 type Member struct {
 	Id            int       `gorm:"primary_key"json:"id"`
 	Uuid          uuid.UUID `gorm:"type:varchar(128);not null"json:"uuid"`
-	Type          uuid.UUID `gorm:"type:varchar(128);not null"json:"type"`
 	UserUuid      uuid.UUID `gorm:"type:varchar(128);not null;index:user_uuid"json:"user_uuid"`
 	PrincipalUuid uuid.UUID `gorm:"type:varchar(128);not null;index:principal_uuid"json:"principal_uuid"`
 	CreatedAt     time.Time `json:"created_at"`
