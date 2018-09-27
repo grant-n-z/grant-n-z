1a81c8eea2e97b15366fb7042e93aea5ae1122ff
tomoyane <tohito0430@gmail.com>
2018-09-28 00:10:07 +0900

    fix principal service

--- domain/service/principal_service.go
+++ domain/service/principal_service.go
@@ -4,6 +4,7 @@ import (
 	"github.com/tomoyane/grant-n-z/domain/entity"
 	"github.com/tomoyane/grant-n-z/domain/repository"
 	"github.com/satori/go.uuid"
+	"github.com/tomoyane/grant-n-z/handler"
 )
 
 type PrincipalService struct {
@@ -14,7 +15,25 @@ func (s PrincipalService) GetPrincipalByName(name string) *entity.Principal {
 	return s.PrincipalRepository.FindByName(name)
 }
 
-func (s PrincipalService) InsertRole(principal entity.Principal) *entity.Principal {
+func (s PrincipalService) InsertPrincipal(principal entity.Principal) *entity.Principal {
 	principal.Uuid, _ = uuid.NewV4()
 	return s.PrincipalRepository.Save(principal)
+}
+
+func (s PrincipalService) PostPrincipalData(principal *entity.Principal) (insertedPrincipal *entity.Principal, errRes *handler.ErrorResponse) {
+	principalData := s.GetPrincipalByName(principal.Name)
+	if principalData == nil {
+		return nil, handler.InternalServerError("")
+	}
+
+	if len(principalData.Name) > 0 {
+		return nil, handler.Conflict("")
+	}
+
+	principalData = s.InsertPrincipal(*principal)
+	if principalData == nil {
+		return nil, handler.InternalServerError("")
+	}
+
+	return principalData, nil
 }
