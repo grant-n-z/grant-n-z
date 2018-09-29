bad76e1b7f23fcfd8016ca781dcb035bb1002bca
tomoyane <tohito0430@gmail.com>
2018-09-30 00:29:56 +0900

    add member service di

--- di/component.go
+++ di/component.go
@@ -7,45 +7,52 @@ import (
 )
 
 var (
-	ProviderUserService service.UserService
-	ProviderTokenService service.TokenService
-	ProviderRoleService service.RoleService
-	ProviderPrincipalService service.PrincipalService
-	ProviderServiceService service.ServiceService
+	ProvideUserService      service.UserService
+	ProvideTokenService     service.TokenService
+	ProvideRoleService      service.RoleService
+	ProvidePrincipalService service.PrincipalService
+	ProvideServiceService   service.ServiceService
+	ProvideMemberService    service.MemberService
 )
 
 func InitUserService(repo repository.UserRepository) {
-	ProviderUserService = service.UserService{
+	ProvideUserService = service.UserService{
 		UserRepository: repo,
 	}
 }
 
 func InitTokenService(tRepo repository.TokenRepository, uRepo repository.UserRepository) {
-	ProviderTokenService = service.TokenService{
+	ProvideTokenService = service.TokenService{
 		TokenRepository: tRepo,
-		UserRepository: uRepo,
+		UserRepository:  uRepo,
 	}
 }
 
 func InitRoleService(repo repository.RoleRepository) {
-	ProviderRoleService = service.RoleService{
+	ProvideRoleService = service.RoleService{
 		RoleRepository: repo,
 	}
 }
 
 func InitPrincipalService(pRepo repository.PrincipalRepository, uRepo repository.UserRepository,
 	sRepo repository.ServiceRepository, mRepo repository.MemberRepository, rRepo repository.RoleRepository) {
-	ProviderPrincipalService = service.PrincipalService{
+	ProvidePrincipalService = service.PrincipalService{
 		PrincipalRepository: pRepo,
-		UserRepository: uRepo,
-		ServiceRepository: sRepo,
-		MemberRepository: mRepo,
-		RoleRepository: rRepo,
+		UserRepository:      uRepo,
+		ServiceRepository:   sRepo,
+		MemberRepository:    mRepo,
+		RoleRepository:      rRepo,
 	}
 }
 
 func InitServiceService(repo repository.ServiceRepository) {
-	ProviderServiceService = service.ServiceService{
+	ProvideServiceService = service.ServiceService{
 		ServiceRepository: repo,
+	}
+}
+
+func InitMemberService(repo repository.MemberRepository) {
+	ProvideMemberService = service.MemberService{
+		MemberRepository: repo,
 	}
 }
