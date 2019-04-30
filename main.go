c1647466a94f35ee44b92377ebf41e3ab376f174
tomoyane <tohito0430@gmail.com>
2019-04-30 15:58:20 +0900

    add role, service

--- main.go
+++ main.go
@@ -17,7 +17,12 @@ func main() {
 	fmt.Println("start grant-n-z server :8080")
 
 	userHandler := handler.NewUserHandler()
+	serviceHandler := handler.NewServiceHandler()
+	roleHandler := handler.NewRoleHandler()
+
 	http.HandleFunc("/api/v1/users",  userHandler.Post)
+	http.HandleFunc("/api/v1/services",  serviceHandler.Post)
+	http.HandleFunc("/api/v1/roles",  roleHandler.Post)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
