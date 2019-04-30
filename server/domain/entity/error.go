d39b30684b46e6e23fc6979bd44a173c18024f10
tomoyane <tohito0430@gmail.com>
2019-04-30 15:59:16 +0900

    fix error res

--- server/domain/entity/error.go
+++ server/domain/entity/error.go
@@ -6,67 +6,108 @@ import (
 )
 
 type ErrorResponse struct {
-	Code    int    `json:"code"`
-	Message string `json:"message"`
-}
-
-func BadRequest() *ErrorResponse {
-	return &ErrorResponse{
-		Code: http.StatusBadRequest,
-		Message: "Bad request.",
-	}
-}
-
-func Unauthorized() ErrorResponse {
-	return ErrorResponse{
-		Code: http.StatusUnauthorized,
-		Message: "Unauthorized.",
-	}
-}
-
-func Forbidden() ErrorResponse {
-	return ErrorResponse{
-		Code: http.StatusForbidden,
-		Message: "Forbidden.",
-	}
-}
-
-func NotFound() ErrorResponse {
-	return ErrorResponse{
-		Code: http.StatusNotFound,
-		Message: "Not found.",
-	}
-}
-
-func Conflict() *ErrorResponse {
-	return &ErrorResponse{
-		Code: http.StatusConflict,
-		Message: "Conflict.",
-	}
-}
-
-func MethodNotAllowed() *ErrorResponse {
-	return &ErrorResponse{
-		Code: http.StatusMethodNotAllowed,
-		Message: "Method Not Allowed.",
-	}
-}
-
-func UnProcessableEntity() ErrorResponse {
-	return ErrorResponse{
-		Code: http.StatusUnprocessableEntity,
-		Message: "UnProcessable Entity.",
-	}
-}
-
-func InternalServerError() *ErrorResponse {
-	return &ErrorResponse{
-		Code: http.StatusInternalServerError,
-		Message: "Internal server error.",
-	}
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Detail  interface{} `json:"detail"`
 }
 
 func (er ErrorResponse) ToJson() string {
 	jsonBytes, _ := json.Marshal(er)
 	return string(jsonBytes)
+}
+
+func BadRequest(err ...string) *ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return &ErrorResponse{
+		Code:    http.StatusBadRequest,
+		Message: "Bad request.",
+		Detail:  detail,
+	}
+}
+
+func Unauthorized(err ...string) ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return ErrorResponse{
+		Code:    http.StatusUnauthorized,
+		Message: "Unauthorized.",
+		Detail:  detail,
+	}
+}
+
+func Forbidden(err ...string) ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return ErrorResponse{
+		Code:    http.StatusForbidden,
+		Message: "Forbidden.",
+		Detail:  detail,
+	}
+}
+
+func NotFound(err ...string) ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return ErrorResponse{
+		Code:    http.StatusNotFound,
+		Message: "Not found.",
+		Detail:  detail,
+	}
+}
+
+func Conflict(err ...string) *ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return &ErrorResponse{
+		Code:    http.StatusConflict,
+		Message: "Conflict.",
+		Detail:  detail	,
+	}
+}
+
+func MethodNotAllowed(err ...string) *ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return &ErrorResponse{
+		Code:    http.StatusMethodNotAllowed,
+		Message: "Method Not Allowed.",
+		Detail:  detail,
+	}
+}
+
+func UnProcessableEntity(err ...string) ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return ErrorResponse{
+		Code:    http.StatusUnprocessableEntity,
+		Message: "UnProcessable Entity.",
+		Detail:  detail,
+	}
+}
+
+func InternalServerError(err ...string) *ErrorResponse {
+	var detail string
+	if err != nil {
+		detail = err[0]
+	}
+	return &ErrorResponse{
+		Code:    http.StatusInternalServerError,
+		Message: "Internal server error.",
+		Detail:  detail,
+	}
 }
