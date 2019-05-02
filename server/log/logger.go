1b246fbb8518e16740fb33932d2d5a072cab9065
tomoyane <tohito0430@gmail.com>
2019-05-02 15:19:13 +0900

    fix logger

--- server/log/logger.go
+++ server/log/logger.go
@@ -3,32 +3,47 @@ package log
 import (
 	"log"
 	"os"
+	"strings"
 )
 
-var Logger = NewLogger()
+var Logger Log
 
 type Log struct {
-	i *log.Logger
-	w *log.Logger
-	e *log.Logger
+	level string
+	d     *log.Logger
+	i     *log.Logger
+	w     *log.Logger
+	e     *log.Logger
 }
 
-func NewLogger() Log {
-	return Log{
-		i: log.New(os.Stdout, "[INFO]", log.LstdFlags|log.LUTC),
-		w: log.New(os.Stderr, "[WARN]", log.LstdFlags|log.LUTC),
-		e: log.New(os.Stderr, "[ERROR]", log.LstdFlags|log.LUTC),
+func InitLogger(logLevel string) {
+	Logger = Log{
+		level: logLevel,
+		d:     log.New(os.Stdout, "[DEBUG]", log.LstdFlags|log.LUTC),
+		i:     log.New(os.Stdout, "[INFO]", log.LstdFlags|log.LUTC),
+		w:     log.New(os.Stderr, "[WARN]", log.LstdFlags|log.LUTC),
+		e:     log.New(os.Stderr, "[ERROR]", log.LstdFlags|log.LUTC),
 	}
 }
 
-func (l Log) Info(message ...interface{}) {
-	l.i.Println(message...)
+func (l Log) Fatal(v ...interface{}) {
+	log.Fatal(v)
 }
 
-func (l Log) Warn(message ...interface{}) {
-	l.w.Println(message...)
+func (l Log) Debug(log ...interface{}) {
+	if strings.EqualFold(l.level, "DEBUG") || strings.EqualFold(l.level, "debug") {
+		l.d.Println(log...)
+	}
 }
 
-func (l Log) Error(message ...interface{}) {
-	l.e.Println(message...)
+func (l Log) Info(log ...interface{}) {
+	l.i.Println(log...)
+}
+
+func (l Log) Warn(log ...interface{}) {
+	l.w.Println(log...)
+}
+
+func (l Log) Error(log ...interface{}) {
+	l.e.Println(log...)
 }
