ea72228d4674bf124c3ed3d256bf8c597ccc28a3
tomoyane <tohito0430@gmail.com>
2019-06-04 19:47:00 +0900

    refactor http main handler

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -6,11 +6,11 @@ import (
 
 	"gopkg.in/go-playground/validator.v9"
 
-	"github.com/tomoyane/grant-n-z/server/entity"
 	"github.com/tomoyane/grant-n-z/server/log"
+	"github.com/tomoyane/grant-n-z/server/model"
 )
 
-func Interceptor(w http.ResponseWriter, r *http.Request) ([]byte, *entity.ErrorResponse) {
+func Interceptor(w http.ResponseWriter, r *http.Request) ([]byte, *model.ErrorResponse) {
 	if err := httpValidator(r); err != nil {
 		log.Logger.Warn("error http validation", err.Detail)
 		http.Error(w, err.ToJson(), err.Code)
@@ -27,29 +27,29 @@ func Interceptor(w http.ResponseWriter, r *http.Request) ([]byte, *entity.ErrorR
 	return bodyBytes, nil
 }
 
-func httpValidator(r *http.Request) *entity.ErrorResponse {
+func httpValidator(r *http.Request) *model.ErrorResponse {
 	if r.Header.Get("Content-Type") != "application/json" {
 		log.Logger.Warn("not allow content-type")
-		return entity.BadRequest()
+		return model.BadRequest()
 	}
 
 	return nil
 }
 
-func bind(r *http.Request) ([]byte, *entity.ErrorResponse) {
+func bind(r *http.Request) ([]byte, *model.ErrorResponse) {
 	body, err := ioutil.ReadAll(r.Body)
 	defer r.Body.Close()
 	if err != nil {
-		return nil, entity.InternalServerError(err.Error())
+		return nil, model.InternalServerError(err.Error())
 	}
 
 	return body, nil
 }
 
-func BodyValidator(w http.ResponseWriter, i interface{}) *entity.ErrorResponse {
+func BodyValidator(w http.ResponseWriter, i interface{}) *model.ErrorResponse {
 	err := validator.New().Struct(i)
 	if err != nil {
-		err := entity.BadRequest(err.Error())
+		err := model.BadRequest(err.Error())
 		log.Logger.Warn("error request validation", err.Detail)
 		http.Error(w, err.ToJson(), err.Code)
 		return err
