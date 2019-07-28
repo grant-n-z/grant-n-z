eb8f5f3910cd0af13f023ec6935b5d69b133766b
tomoyane <tohito0430@gmail.com>
2019-07-28 12:19:57 +0900

    refactor router

--- server/router/router.go
+++ server/router/router.go
@@ -1,9 +1,9 @@
 package router
 
 import (
-	"github.com/tomoyane/grant-n-z/server/handler"
 	"net/http"
 
+	"github.com/tomoyane/grant-n-z/server/handler"
 	"github.com/tomoyane/grant-n-z/server/log"
 )
 
@@ -18,9 +18,22 @@ type Router struct {
 	PolicyHandler             handler.PolicyHandler
 }
 
+func NewRouter() Router {
+	return Router{
+		TokenHandler:              handler.NewTokenHandler(),
+		UserHandler:               handler.NewUserHandler(),
+		ServiceHandler:            handler.NewServiceHandler(),
+		RoleHandler:               handler.NewRoleHandler(),
+		OperatorMemberRoleHandler: handler.NewOperatorMemberRoleHandler(),
+		UserServiceHandler:        handler.NewUserServiceHandler(),
+		PermissionHandler:         handler.NewPermissionHandler(),
+		PolicyHandler:             handler.NewPolicyHandlerHandler(),
+	}
+}
+
 func (r Router) V1() {
 	http.HandleFunc("/api/v1/oauth", r.TokenHandler.Post)
-	http.HandleFunc("/api/v1/users", r.UserServiceHandler.Api)
+	http.HandleFunc("/api/v1/users", r.UserHandler.Api)
 	http.HandleFunc("/api/v1/services", r.ServiceHandler.Api)
 	http.HandleFunc("/api/v1/roles", r.RoleHandler.Api)
 	http.HandleFunc("/api/v1/operator-member-roles", r.OperatorMemberRoleHandler.Api)
@@ -28,17 +41,13 @@ func (r Router) V1() {
 	http.HandleFunc("/api/v1/permissions", r.PermissionHandler.Api)
 	http.HandleFunc("/api/v1/policies", r.PolicyHandler.Api)
 
-	log.Logger.Debug("____ routing info ____")
-	log.Logger.Debug("Method: `POST` routing: /api/v1/oauth")
-	log.Logger.Debug("Method: `POST` routing: /api/v1/users")
-	log.Logger.Debug("Method: `POST`, `GET` Routing: `/api/v1/services`")
-	log.Logger.Debug("Method: `POST`, `GET` Routing: `/api/v1/roles`")
-	log.Logger.Debug("Method: `POST`, `GET` Routing: `/api/v1/operator-member-roles`")
-	log.Logger.Debug("Method: `POST`, `GET` Routing: `/api/v1/user-services`")
-	log.Logger.Debug("Method: `POST`, `GET` Routing: `/api/v1/permissions`")
-	log.Logger.Debug("Method: `POST`, `GET` Routing: `/api/v1/policies`")
-}
-
-func (r Router) Run(port string) {
-	log.Logger.Fatal(http.ListenAndServe(port, nil))
+	log.Logger.Info("------ Routing info ------")
+	log.Logger.Info("HTTP Method: `POST` Routing: /api/v1/oauth")
+	log.Logger.Info("HTTP Method: `POST` Routing: /api/v1/users")
+	log.Logger.Info("HTTP Method: `POST`, `GET` Routing: `/api/v1/services`")
+	log.Logger.Info("HTTP Method: `POST`, `GET` Routing: `/api/v1/roles`")
+	log.Logger.Info("HTTP Method: `POST`, `GET` Routing: `/api/v1/operator-member-roles`")
+	log.Logger.Info("HTTP Method: `POST`, `GET` Routing: `/api/v1/user-services`")
+	log.Logger.Info("HTTP Method: `POST`, `GET` Routing: `/api/v1/permissions`")
+	log.Logger.Info("HTTP Method: `POST`, `GET` Routing: `/api/v1/policies`")
 }
