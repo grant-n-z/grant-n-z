6a2afef5fbe18419951b12b2140df8b1f4f8c858
tomoyane <tohito0430@gmail.com>
2019-07-28 12:15:07 +0900

    refactor main

--- main.go
+++ main.go
@@ -1,12 +1,10 @@
 package main
 
 import (
-	"fmt"
-
 	"github.com/tomoyane/grant-n-z/server/config"
-	"github.com/tomoyane/grant-n-z/server/handler"
 	"github.com/tomoyane/grant-n-z/server/log"
-	"github.com/tomoyane/grant-n-z/server/router"
+
+	gserver "github.com/tomoyane/grant-n-z/server"
 )
 
 func init() {
@@ -16,33 +14,5 @@ func init() {
 }
 
 func main() {
-	cron := handler.NewCronHandler()
-	cron.RunUpdatePolicy()
-
-	route := router.Router{
-		TokenHandler:              handler.NewTokenHandler(),
-		UserHandler:               handler.NewUserHandler(),
-		ServiceHandler:            handler.NewServiceHandler(),
-		RoleHandler:               handler.NewRoleHandler(),
-		OperatorMemberRoleHandler: handler.NewOperatorMemberRoleHandler(),
-		UserServiceHandler:        handler.NewUserServiceHandler(),
-		PermissionHandler:         handler.NewPermissionHandler(),
-		PolicyHandler:             handler.NewPolicyHandlerHandler(),
-	}
-
-	route.V1()
-
-	banner := `start grant-n-z server :8080
-___________________________________________________
-    ____                      _      
-   / __/ _    ____   _____ __//_      _____   ____ 
-  / /__ //__ /__ /  /___ //_ __/     /___ /  /__ /
- / /_ //___///_//_ //  //  //_  === //  // === //__
-/____///   /_____///  //  /__/     //  //     /___/
-___________________________________________________
-High performance authentication and authorization. version is %s
-`
-	fmt.Printf(banner, config.App.Version)
-
-	route.Run(":8080")
+	gserver.NewGrantNZServer().Run(":8080")
 }
