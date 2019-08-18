df3b5ee2dc4d7e2b54a055733fdf051fac89db04
tomoyane <tohito0430@gmail.com>
2019-08-18 22:46:22 +0900

    feat put method

--- gserver/handler/user_impl.go
+++ gserver/handler/user_impl.go
@@ -2,12 +2,12 @@ package handler
 
 import (
 	"encoding/json"
+	"github.com/tomoyane/grant-n-z/gserver/common/property"
 	"net/http"
 
 	"github.com/tomoyane/grant-n-z/gserver/entity"
 	"github.com/tomoyane/grant-n-z/gserver/log"
 	"github.com/tomoyane/grant-n-z/gserver/model"
-
 	"github.com/tomoyane/grant-n-z/gserver/usecase/service"
 )
 
@@ -69,6 +69,34 @@ func (uh UserHandlerImpl) Post(w http.ResponseWriter, r *http.Request) {
 }
 
 func (uh UserHandlerImpl) Put(w http.ResponseWriter, r *http.Request) {
+	log.Logger.Info("PUT users")
+	var userEntity *entity.User
+
+	authUser, err := uh.RequestHandler.VerifyToken(w, r, property.AuthUser)
+	if err != nil {
+		return
+	}
+
+	body, err := uh.RequestHandler.InterceptHttp(w, r)
+	if err != nil {
+		return
+	}
+
+	_ = json.Unmarshal(body, &userEntity)
+	if err := uh.RequestHandler.ValidateHttpRequest(w, userEntity); err != nil {
+		return
+	}
+
+	userEntity.Id = authUser.UserId
+	userEntity.Uuid = authUser.UserUuid
+	if _, err := uh.UserService.UpdateUser(userEntity); err != nil {
+		http.Error(w, err.ToJson(), err.Code)
+		return
+	}
+
+	res, _ := json.Marshal(map[string]string{"message": "User update succeeded."})
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(res)
 }
 
 func (uh UserHandlerImpl) Delete(w http.ResponseWriter, r *http.Request) {
