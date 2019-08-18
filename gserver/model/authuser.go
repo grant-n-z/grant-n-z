9aed27bd3a83579421ee25e9556c73c8a9825c84
tomoyane <tohito0430@gmail.com>
2019-08-18 22:47:22 +0900

    add auth model

---
+++ gserver/model/authuser.go
@@ -0,0 +1,11 @@
+package model
+
+import "github.com/satori/go.uuid"
+
+type AuthUser struct {
+	Username string    `json:"username"`
+	UserUuid uuid.UUID `json:"user_uuid"`
+	UserId   int       `json:"user_id"`
+	Expires  string    `json:"expires"`
+	RoleId   int       `json:"role_id"`
+}
