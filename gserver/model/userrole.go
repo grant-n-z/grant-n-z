f6e0a2a7adcae6c73178610c8df4f33fa812d69a
tomoyane <tohito0430@gmail.com>
2019-08-20 23:23:52 +0900

    feat join model

---
+++ gserver/model/userrole.go
@@ -0,0 +1,20 @@
+package model
+
+import (
+	"time"
+
+	"github.com/satori/go.uuid"
+)
+
+// User entity with OperatorMemberRole entity
+type UserOperatorMemberRole struct {
+	Id        int       `json:"id"`
+	Uuid      uuid.UUID `json:"uuid"`
+	Username  string    `json:"username"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	RoleId    int       `json:"role_id"`
+	UserId    int       `json:"user_id"`
+}
