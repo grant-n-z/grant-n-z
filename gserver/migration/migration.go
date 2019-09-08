35c422dc39708354f322d7f32461a8fc959770b6
tomoyane <tohito0430@gmail.com>
2019-09-08 21:12:58 +0900

    refactor injection

--- gserver/migration/migration.go
+++ gserver/migration/migration.go
@@ -9,16 +9,16 @@ import (
 )
 
 type Migration struct {
-	UserService               service.UserService
-	RoleService               service.RoleService
-	OperatorMemberRoleService service.OperatorMemberRoleService
+	UserService           service.UserService
+	RoleService           service.RoleService
+	OperatorPolicyService service.OperatorPolicyService
 }
 
 func NewMigration() Migration {
 	return Migration{
-		UserService:               service.GetUserServiceInstance(),
-		RoleService:               service.GetRoleServiceInstance(),
-		OperatorMemberRoleService: service.GetOperatorMemberRoleServiceInstance(),
+		UserService:           service.GetUserServiceInstance(),
+		RoleService:           service.GetRoleServiceInstance(),
+		OperatorPolicyService: service.GetOperatorPolicyServiceInstance(),
 	}
 }
 
@@ -56,17 +56,17 @@ func (m Migration) V1() {
 	log.Logger.Info("Generate to role for migration")
 
 	// Generate operator operator_member_role
-	operatorMemberRole := entity.OperatorMemberRole{
+	operatorMemberRole := entity.OperatorPolicy{
 		UserId: 1,
 		RoleId: 1,
 	}
-	_, operatorRoleMemberErr := m.OperatorMemberRoleService.Insert(&operatorMemberRole)
+	_, operatorRoleMemberErr := m.OperatorPolicyService.Insert(&operatorMemberRole)
 	if operatorRoleMemberErr != nil {
 		if userErr.Code != http.StatusConflict {
-			log.Logger.Fatal("Error generate operator_member_role for migration")
+			log.Logger.Fatal("Error generate operator_policies for migration")
 		}
 	}
-	log.Logger.Info("Generate to operator_member_role for migration")
+	log.Logger.Info("Generate to operator_policies for migration")
 }
 
 func (m Migration) checkAdminUser() bool {
@@ -79,8 +79,8 @@ func (m Migration) checkAdminUser() bool {
 		log.Logger.Fatal("Failed to not valid grant_n_z schema or data is broken for migration")
 	}
 
-	var operatorAdminMemberRole []*entity.OperatorMemberRole
-	operatorAdminMemberRole, err = m.OperatorMemberRoleService.GetByUserId(1)
+	var operatorAdminMemberRole []*entity.OperatorPolicy
+	operatorAdminMemberRole, err = m.OperatorPolicyService.GetByUserId(1)
 	if err != nil {
 		log.Logger.Fatal("Failed to not valid grant_n_z schema or data is broken for migration")
 	}
