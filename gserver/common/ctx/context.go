d24ce2aa104f6a219bb87d04208d802c885fd64c
tomoyane <tohito0430@gmail.com>
2019-10-22 13:51:12 +0900

    feat request scope

--- gserver/common/ctx/context.go
+++ gserver/common/ctx/context.go
@@ -9,17 +9,33 @@ func InitContext() {
 }
 
 func SetApiKey(apiKey string) {
-	Ctx = context.WithValue(Ctx, "Api-Key", apiKey)
+	Ctx = context.WithValue(Ctx, "api_key", apiKey)
 }
 
 func GetApiKey() interface{} {
-	return Ctx.Value("Api-Key")
+	return Ctx.Value("api_key")
 }
 
-func SetToken(token string) {
-	Ctx = context.WithValue(Ctx, "Token", token)
+func SetServiceId(serviceId int) {
+	Ctx = context.WithValue(Ctx, "service_id", serviceId)
 }
 
-func GetToken() interface{} {
-	return Ctx.Value("Token")
+func GetServiceId() interface{} {
+	return Ctx.Value("service_id")
+}
+
+func SetUserId(userId int) {
+	Ctx = context.WithValue(Ctx, "user_id", userId)
+}
+
+func GetUserId() interface{} {
+	return Ctx.Value("user_id")
+}
+
+func SetUserUuid(userUuid string) {
+	Ctx = context.WithValue(Ctx, "user_uuid", userUuid)
+}
+
+func GetUserUuid() interface{} {
+	return Ctx.Value("user_uuid")
 }
