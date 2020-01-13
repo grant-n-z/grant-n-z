f909a05d1c947e9213796a2d82c33e25840d0ea7
tomoyane <tohito0430@gmail.com>
2020-01-13 14:06:05 +0900

    refactor package name

---
+++ gserver/common/constant/constant.go
@@ -0,0 +1,13 @@
+package constant
+
+// System constant
+const (
+	Test           = "test"
+	AppEnv         = "APP_ENV"
+	OperatorRoleId = 1
+	AuthOperator   = "operator"
+	AuthUser       = "user"
+	Operator       = "operator"
+	Admin          = "admin"
+	AdminPolicy    = "admin_policy"
+)
