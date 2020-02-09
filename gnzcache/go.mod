69db161d6b21cd888900006daea413ba4a58b602
tomoyane <tohito0430@gmail.com>
2020-02-09 20:27:46 +0900

    refactor mod

---
+++ gnzcache/go.mod
@@ -0,0 +1,5 @@
+module github.com/tomoyane/grant-n-z/gnzcache
+
+go 1.13
+
+require github.com/tomoyane/grant-n-z v0.0.0-20200209094014-39d795473169 // indirect
--- main.go
+++ main.go
@@ -4,7 +4,12 @@ import (
 	"github.com/tomoyane/grant-n-z/gnzserver"
 )
 
-// Example
-func main() {
+// Example GrantNZ server
+func server() {
+	gnzserver.NewGrantNZServer().Run()
+}
+
+// Example GrantNZ cache
+func cache() {
 	gnzserver.NewGrantNZServer().Run()
 }
