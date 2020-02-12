fc8552be69540f5efcb0a60d471d700a1d691426
tomoyane <tohito0430@gmail.com>
2020-02-12 23:46:41 +0900

    add timer interface

---
+++ gnzcache/timer/timer.go
@@ -0,0 +1,55 @@
+package timer
+
+import (
+	"time"
+
+	"github.com/tomoyane/grant-n-z/gnz/log"
+)
+
+// UpdateTimer interface
+type UpdateTimer interface {
+	// Start update cache timer
+	Start(exitCode chan int) int
+
+	// Stop update cache timer
+	Stop()
+}
+
+// UpdateTimer struct
+type UpdateTimerImpl struct {
+	Ticker    *time.Ticker
+	IsRunning bool
+}
+
+// Constructor
+func NewUpdateTimer() UpdateTimer {
+	return UpdateTimerImpl{
+		Ticker:    time.NewTicker(5 * time.Minute),
+		IsRunning: true,
+	}
+}
+
+// Start cache update timer
+func (ut UpdateTimerImpl) Start(exitCode chan int) int {
+	code := 0
+
+	loop:
+		for {
+			select {
+			case <-ut.Ticker.C:
+				// TODO
+			case c := <-exitCode:
+				log.Logger.Info("Break update cache loop")
+				ut.Ticker.Stop()
+				code = c
+				break loop
+			}
+		}
+
+	log.Logger.Info("Stopped update cache process")
+	return code
+}
+
+func (ut UpdateTimerImpl) Stop() {
+	ut.Ticker.Stop()
+}
