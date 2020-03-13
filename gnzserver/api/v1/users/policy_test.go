8aee8c6b1ab5b239757e815bdd54cf77a787b6a8
tomoyane <tomoyane111@gmail.com>
2020-03-13 23:40:51 +0900

    refactor test

--- gnzserver/api/v1/users/policy_test.go
+++ gnzserver/api/v1/users/policy_test.go
@@ -1,48 +1,6 @@
 package users
 
-import (
-	"fmt"
-	"strings"
-	"testing"
-
-	"encoding/json"
-	"net/http"
-	"net/http/httptest"
-
-	"github.com/google/uuid"
-	"github.com/stretchr/testify/assert"
-
-	"github.com/tomoyane/grant-n-z/gnz/entity"
-)
-
-const (
-	endpointPolicies = "/api/v1/policies"
-)
+import "testing"
 
 func TestPolicyHandlerGet(t *testing.T) {
-	request := httptest.NewRequest(http.MethodGet, endpointPolicies, nil)
-	recorder := httptest.NewRecorder()
-
-	NewPolicyHandler().Get(recorder, request)
-	assert.Equal(t, http.StatusOK, recorder.Code)
-}
-
-func TestPolicyHandlerPost(t *testing.T) {
-	id, _ := uuid.NewV4()
-	name := fmt.Sprintf("unit_test_%s", id.String())
-
-	policy := entity.Policy{
-		Name:                name,
-		PermissionId:        1,
-		ServiceMemberRoleId: 1,
-	}
-
-	body, _ := json.Marshal(policy)
-
-	request := httptest.NewRequest(http.MethodPost, endpointPolicies, strings.NewReader(string(body)))
-	request.Header.Set("Content-Type", "application/json")
-	recorder := httptest.NewRecorder()
-
-	NewPolicyHandler().Post(recorder, request)
-	assert.Equal(t, http.StatusCreated, recorder.Code)
 }
