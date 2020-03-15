15782e6a99d1fceb3ae85e3316b390b3928747cf
tomoyane <tomoyane111@gmail.com>
2020-03-15 21:30:47 +0900

    add server build

--- .github/script/build.sh
+++ .github/script/build.sh
@@ -6,3 +6,8 @@ cd gnzcacher
 docker login docker.pkg.github.com -u "${DOCKER_USER}" -p "${GITHUB_TOKEN}"
 docker build -t docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:latest .
 docker push docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:latest
+
+cd ../gnzserver
+docker login docker.pkg.github.com -u "${DOCKER_USER}" -p "${GITHUB_TOKEN}"
+docker build -t docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:latest .
+docker push docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:latest
