9cdcd2ff84e1d9ddf903bb213eb094e02030aa2b
tomoyane <tomoyane111@gmail.com>
2020-03-15 18:51:12 +0900

    fix env

--- .github/script/build.sh
+++ .github/script/build.sh
@@ -3,6 +3,6 @@
 set -e -u -x
 
 cd gnzcacher
-docker login -u ${{ secrets.DOCKER_USER }} -p ${{ secrets.DOCKER_PASSWORD }}
+docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
 docker build -t tomohito/gnzcacher:latest .
 docker push tomohito/gnzcacher:latest
