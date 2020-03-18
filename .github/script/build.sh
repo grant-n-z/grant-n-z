9ac282a3b12287eaceb8881b8603399e55cb4e65
tomoyane <tomoyane111@gmail.com>
2020-03-18 22:58:47 +0900

    test

--- .github/script/build.sh
+++ .github/script/build.sh
@@ -2,12 +2,20 @@
 
 set -e -u -x
 
+# gnzcacher build
 cd gnzcacher
+cat grant_n_z_cacher.yaml| grep version | sed 's/^[ \t]*//' | sed 's/version://' | sed 's/^[ \t]*//' > version
+ver=`cat version`
 docker login docker.pkg.github.com -u "${DOCKER_USER}" -p "${GITHUB_TOKEN}"
-docker build -t docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:latest .
+docker build -t docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:"${ver}" -t docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:latest .
+docker push docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:"${ver}"
 docker push docker.pkg.github.com/tomoyane/grant-n-z/gnzcacher:latest
 
+# gnzserver build
 cd ../gnzserver
+cat grant_n_z_server.yaml| grep version | sed 's/^[ \t]*//' | sed 's/version://' | sed 's/^[ \t]*//' > version
+ver=`cat version`
 docker login docker.pkg.github.com -u "${DOCKER_USER}" -p "${GITHUB_TOKEN}"
-docker build -t docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:latest .
+docker build -t docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:"${ver}" -t docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:latest .
+docker push docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:"${ver}"
 docker push docker.pkg.github.com/tomoyane/grant-n-z/gnzserver:latest
