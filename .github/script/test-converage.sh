9a706cbb69d5e27a51b29916aa8090087bce5633
tomoyane <tomoyane111@gmail.com>
2020-04-04 15:59:01 +0900

    fix job

--- .github/script/test-converage.sh
+++ .github/script/test-converage.sh
@@ -2,4 +2,6 @@
 
 set -e -u -x
 
-go test -coverprofile=coverage.out -v ./...
+go test -race -covermode atomic -coverprofile=profile.cov ./...
+GO111MODULE=off go get github.com/mattn/goveralls
+$(go env GOPATH)/bin/goveralls -coverprofile=profile.cov -service=github
