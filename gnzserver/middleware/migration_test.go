d48d9fd99a120c03a5b59db3753c20b5e61ec135
tomoyane <tomoyane111@gmail.com>
2020-04-12 15:26:57 +0900

    add middleware test

--- gnzserver/middleware/migration_test.go
+++ gnzserver/middleware/migration_test.go
@@ -1,1 +1,84 @@
 package middleware
+
+import (
+	"testing"
+	"time"
+
+	"go.etcd.io/etcd/clientv3"
+
+	"github.com/jinzhu/gorm"
+	"github.com/tomoyane/grant-n-z/gnz/cache"
+	"github.com/tomoyane/grant-n-z/gnz/ctx"
+	"github.com/tomoyane/grant-n-z/gnz/log"
+	"github.com/tomoyane/grant-n-z/gnzserver/service"
+)
+
+var migration Migration
+
+func init() {
+	log.InitLogger("info")
+	ctx.InitContext()
+
+	stubConnection, _ := gorm.Open("sqlite3", "/tmp/test_grant_nz.db")
+	stubEtcdConnection, _ := clientv3.New(clientv3.Config{
+		Endpoints:            []string{},
+		DialTimeout:          5 * time.Millisecond,
+		DialKeepAliveTimeout: 5 * time.Millisecond,
+	})
+
+	userService := service.UserServiceImpl{
+		UserRepository: StubUserRepositoryImpl{Connection: stubConnection},
+		EtcdClient: cache.EtcdClientImpl{
+			Connection: stubEtcdConnection,
+			Ctx:        ctx.GetCtx(),
+		},
+	}
+
+	operatorPolicyService := service.OperatorPolicyServiceImpl{
+		OperatorPolicyRepository: StubOperatorPolicyRepositoryImpl{Connection: stubConnection},
+		UserRepository:           StubUserRepositoryImpl{Connection: stubConnection},
+		RoleRepository:           StubRoleRepositoryImpl{Connection: stubConnection},
+	}
+
+	ser := service.ServiceImpl{
+		EtcdClient: cache.EtcdClientImpl{
+			Connection: stubEtcdConnection,
+			Ctx:        ctx.GetCtx(),
+		},
+		ServiceRepository:    StubServiceRepositoryImpl{Connection: stubConnection},
+		RoleRepository:       StubRoleRepositoryImpl{Connection: stubConnection},
+		PermissionRepository: StubPermissionRepositoryImpl{Connection: stubConnection},
+	}
+
+	roleService := service.RoleServiceImpl{
+		EtcdClient: cache.EtcdClientImpl{
+			Connection: stubEtcdConnection,
+			Ctx:        ctx.GetCtx(),
+		},
+		RoleRepository: StubRoleRepositoryImpl{Connection: stubConnection},
+	}
+
+	permissionService := service.PermissionServiceImpl{
+		EtcdClient: cache.EtcdClientImpl{
+			Connection: stubEtcdConnection,
+			Ctx:        ctx.GetCtx(),
+		},
+		PermissionRepository: StubPermissionRepositoryImpl{Connection: stubConnection},
+	}
+
+	migration = Migration{
+		service:               ser,
+		userService:           userService,
+		roleService:           roleService,
+		operatorPolicyService: operatorPolicyService,
+		permissionService:     permissionService,
+	}
+}
+
+func TestNewMigration(t *testing.T) {
+	NewMigration()
+}
+
+func TestMigration_V1(t *testing.T) {
+	migration.V1()
+}
