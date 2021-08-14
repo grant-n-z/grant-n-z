d2137166eea34c5bd77870d36338f0062c7128fa
tomoyane <tomoyane111@gmail.com>
2021-08-14 17:49:52 +0900

    [ci skip] update github action build

--- .github/script/build.sh
+++ .github/script/build.sh
@@ -5,14 +5,14 @@ set -e -u -x
 # gnzcacher build
 now=$(date "+%Y%m%d%H%M%S")
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker build -t grantnz/gnzcacher:"${now}" .
+docker build -f gnzcacher/Dockerfile -t grantnz/gnzcacher:"${now}" .
 docker push grantnz/gnzcacher:"${now}"
-docker build -t grantnz/gnzcacher:latest .
+docker build -f gnzcacher/Dockerfile -t grantnz/gnzcacher:latest .
 docker push grantnz/gnzcacher:latest
 
 # gnzserver build
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker build -t grantnz/gnzserver:"${now}" .
+docker build -f gnzserver/Dockerfile -t grantnz/gnzserver:"${now}" .
 docker push grantnz/gnzserver:"${now}"
-docker build -t grantnz/gnzserver:latest .
+docker build -f gnzserver/Dockerfile -t grantnz/gnzserver:latest .
 docker push grantnz/gnzserver:latest
