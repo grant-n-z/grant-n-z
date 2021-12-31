7b299e23944f3381382e63daa24d01d43a9b8078
tomoyane <tomoyane111@gmail.com>
2021-12-31 16:12:43 +0900

    [ci skip] fix release script

--- .github/scripts/build_release.sh
+++ .github/scripts/build_release.sh
@@ -3,8 +3,8 @@
 set -eu
 
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker build -t grantnz/gnzcacher:"${TAG_NAME}" .
+docker build -f gnzcacher/Dockerfile -t grantnz/gnzcacher:"${TAG_NAME}" .
 docker push grantnz/gnzcacher:"${TAG_NAME}"
 
-docker build -t grantnz/gnzserver:"${TAG_NAME}" .
+docker build -f gnzserver/Dockerfile -t grantnz/gnzserver:"${TAG_NAME}" .
 docker push grantnz/gnzserver:"${TAG_NAME}"
